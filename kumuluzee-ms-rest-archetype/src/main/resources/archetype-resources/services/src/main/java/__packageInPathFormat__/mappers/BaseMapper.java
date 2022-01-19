0f34f991a755d7c38bff2b9515f28b34bfee325d
Miha Jamsek <miha_jamsek@windowslive.com>
2022-01-19 17:58:27 +0100

    updated common code

---
+++ kumuluzee-ms-rest-archetype/src/main/resources/archetype-resources/services/src/main/java/__packageInPathFormat__/mappers/BaseMapper.java
@@ -0,0 +1,29 @@
+package ${package}.mappers;
+
+import ${package}.lib.BaseType;
+import ${package}.persistence.BaseEntity;
+
+public class BaseMapper {
+    
+    private BaseMapper() {
+    
+    }
+    
+    public static <T extends BaseType, E extends BaseEntity> T fromEntity(E entity, Class<T> typeClass) {
+        try {
+            T instance = typeClass.getDeclaredConstructor().newInstance();
+            return fromEntity(entity, instance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public static <T extends BaseType, E extends BaseEntity> T fromEntity(E entity, T type) {
+        type.setId(entity.getId());
+        if (entity.getTimestamp() != null) {
+            type.setTimestamp(entity.getTimestamp());
+        }
+        return type;
+    }
+    
+}
--- kumuluzee-ms-rest-archetype/src/main/resources/archetype-resources/v1/api/src/main/java/__packageInPathFormat__/api/providers/ObjectMapperProvider.java
+++ kumuluzee-ms-rest-archetype/src/main/resources/archetype-resources/v1/api/src/main/java/__packageInPathFormat__/api/providers/ObjectMapperProvider.java
@@ -1,24 +1,16 @@
 package ${package}.api.providers;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import ${package}.producers.JacksonProducer;
 
 import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
     
     @Override
     public ObjectMapper getContext(Class<?> aClass) {
-        
-        ObjectMapper mapper = new ObjectMapper();
-        
-        mapper.registerModule(new JavaTimeModule());
-        
-        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        
-        return mapper;
+        return JacksonProducer.getNewMapper();
     }
 }
